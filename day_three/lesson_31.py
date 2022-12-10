ca3e87b7eecaaa74973351a2023e4622db967b49
tkpguy <116381625+pvrklrd@users.noreply.github.com>
2022-12-10 23:42:22 +0530

    update lsn31

---
+++ day_three/lesson_31.py
@@ -0,0 +1,18 @@
+"""
+if height is greater than 120cm then can ride and buy ticket. 
+ticket costs $7 for children below or at 18. ticket cost is $12 if the age is grater tha 18.
+finally, children below 120cm height are not allowed.
+"""
+age = int(input("\n\n\tEnter your age : "))
+name = input("\tYour name please : ")
+height = int(input("\tWhat's your height in centimeter? : "))
+
+if(height > 120):
+    print(f"\n\tHey {name} you can ride and get the ticket")
+    if(age > 18):
+        print(f"\tYour ticket cost is $12\n")
+    else:
+        print(f"\tYour ticket cost is $7\n")
+else:
+    print(f"\n\tHey {name} you can't ride. You gotta grow up!\n")
+    
